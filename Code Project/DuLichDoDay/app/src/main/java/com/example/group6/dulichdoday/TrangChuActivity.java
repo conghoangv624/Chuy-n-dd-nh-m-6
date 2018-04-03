5df7731ac12e414ac6f73bdcacb62096dd2f212b
Hoàng Nghiệp <hoangnghiep169@gmail.com>
2018-04-03 16:36:04 +0700

    project template

---
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/TrangChuActivity.java
@@ -0,0 +1,13 @@
+package com.example.group6.dulichdoday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrangChuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trang_chu_layout);
+    }
+}
