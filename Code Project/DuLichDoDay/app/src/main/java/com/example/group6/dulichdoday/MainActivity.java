bfa39d1ac12769ceb655740e69b77ab09769831a
thphuc202 <33826347+thphuc202@users.noreply.github.com>
2018-04-04 16:43:29 +0700

    fix project

--- Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/MainActivity.java
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.group6.dulichdoday;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HangDuLichActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hang_du_lich_layout);
+        setContentView(R.layout.activity_main);
     }
 }
