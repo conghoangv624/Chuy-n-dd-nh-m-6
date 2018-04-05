ba7bd948351ab5b812b157db43f6724ab934a0d8
Hoàng Nghiệp <hoangnghiep169@gmail.com>
2018-04-05 19:40:09 +0700

    to root

---
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/MainLayoutActivity.java
@@ -0,0 +1,47 @@
+package com.example.group6.dulichdoday;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainLayoutActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_new:
+                    mTextMessage.setText(R.string.title_new);
+                    return true;
+                case R.id.navigation_tour:
+                    mTextMessage.setText(R.string.title_tour);
+                    return true;
+                case R.id.navigation_users:
+                    mTextMessage.setText(R.string.title_user);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
