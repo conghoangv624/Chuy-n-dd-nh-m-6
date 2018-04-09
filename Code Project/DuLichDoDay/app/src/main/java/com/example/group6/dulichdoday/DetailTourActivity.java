352b35e4911b15a94ea09e1b7873aed69a12bc16
thphuc202 <33826347+thphuc202@users.noreply.github.com>
2018-04-09 10:46:17 +0700

    uodate home and tour
    tour detail and home screen

---
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/DetailTourActivity.java
@@ -0,0 +1,13 @@
+package com.example.group6.dulichdoday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailTourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_tour_layout);
+    }
+}
