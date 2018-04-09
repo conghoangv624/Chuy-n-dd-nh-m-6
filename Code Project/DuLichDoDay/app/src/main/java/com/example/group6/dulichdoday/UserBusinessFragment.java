51c550755fd406012795f1cb10a20e6fa96dc699
Hoàng Nghiệp <hoangnghiep169@gmail.com>
2018-04-09 09:57:29 +0700

    fix project
    Revert version

--- Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/UserBusinessFragment.java
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/UserBusinessFragment.java
@@ -1,21 +1,5 @@
 package com.example.group6.dulichdoday;
 
-<<<<<<< HEAD
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
-public class UserBusinessFragment extends Fragment {
-
-    private LinearLayout personal;
-    private LinearLayout manager;
-    private LinearLayout password;
-=======
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -28,7 +12,6 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class UserBusinessFragment extends Fragment {
->>>>>>> 58640c0e7a4f6309d25da934b7a7440eb7e04583
 
 
     public UserBusinessFragment() {
@@ -40,42 +23,7 @@ public class UserBusinessFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-<<<<<<< HEAD
-        View view = inflater.inflate(R.layout.fragment_user_business, container, false);
-
-        personal = (LinearLayout) view.findViewById(R.id.linear_personal);
-        manager = (LinearLayout) view.findViewById(R.id.linear_product_manager);
-        password = (LinearLayout) view.findViewById(R.id.linear_pass);
-
-        manager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent inManager = new Intent(getContext(),UserBusinessFragment.class);
-                startActivity(inManager);
-            }
-        });
-
-        password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent inpersonal = new Intent(getContext(),PasswordActivity.class);
-                startActivity(inpersonal);
-            }
-        });
-
-        personal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent inBusiness = new Intent(getContext(),DetailBusinessActivity.class);
-                startActivity(inBusiness);
-            }
-        });
-
-        return view;
-    }
-=======
         return inflater.inflate(R.layout.fragment_user_business, container, false);
     }
 
->>>>>>> 58640c0e7a4f6309d25da934b7a7440eb7e04583
 }
