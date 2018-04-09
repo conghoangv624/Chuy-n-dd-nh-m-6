59d57e7782ce2f21644900ee2655fcb625462c13
conghoangv624 <conghoangv624@gmail.com>
2018-04-09 17:45:45 +0700

    up date hang du lich
    xem thong tin
    cap nhat thong tin

---
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/DetailBusinessActivity.java
@@ -0,0 +1,17 @@
+package com.example.group6.dulichdoday;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ASUS on 4/9/2018.
+ */
+
+public class DetailBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_business_layout);
+    }
+}
