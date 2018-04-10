00c626272bc8a8d0d00a0518fc02b50207567ad9
hthien <37090960+hthien@users.noreply.github.com>
2018-04-10 23:04:28 +0700

    Update Login and registration activity
    Update Login and registration activity

---
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.group6.dulichdoday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
