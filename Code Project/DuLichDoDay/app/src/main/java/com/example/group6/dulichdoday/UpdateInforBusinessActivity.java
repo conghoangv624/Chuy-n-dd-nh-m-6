512893da3aa827c0c1651e5036ca37ddddf3d2be
conghoangv624 <conghoangv624@gmail.com>
2018-04-18 16:50:55 +0700

    up date cập nhật thông tin hãng du lịch

--- Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/UpdateInforBusinessActivity.java
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/UpdateInforBusinessActivity.java
@@ -1,13 +1,47 @@
 package com.example.group6.dulichdoday;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UpdateInforBusinessActivity extends AppCompatActivity {
-
+    private TextView tvCancle_update;
+    private ImageView img_Choosen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_infor_business_layout);
+        tvCancle_update = (TextView) findViewById(R.id.cancel_update);
+        init();
+
+        final Dialog dialog = new Dialog(UpdateInforBusinessActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); //before
+        dialog.setContentView(R.layout.dialog_choosen);
+
+        img_Choosen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+        tvCancle_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCancel = new Intent(UpdateInforBusinessActivity.this,DetailBusinessActivity.class);
+                startActivity(intentCancel);
+            }
+        });
+
+
+    }
+
+    private void init() {
+        img_Choosen = (ImageView) findViewById(R.id.img_choosen);
     }
 }
