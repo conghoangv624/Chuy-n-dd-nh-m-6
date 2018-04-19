081b74641460d72544dd54c66eacfa1a5f8f2bfd
ngohoagmtoeic1004 <ngohoa.gmtoeic@gmail.com>
2018-04-19 16:24:06 +0700

    update bang tin

--- Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/NewFragment.java
+++ Code Project/DuLichDoDay/app/src/main/java/com/example/group6/dulichdoday/NewFragment.java
@@ -1,34 +1,51 @@
 package com.example.group6.dulichdoday;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ImageWriter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.group6.dulichdoday.Models.New;
 import com.example.group6.dulichdoday.Adapter.AdapterNew;
 import com.example.group6.dulichdoday.Models.New;
 
+import static android.app.Activity.RESULT_OK;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NewFragment extends Fragment {
 
 
     private ArrayList<New> arrayListSP;
     private RecyclerView recyclerView;
     private AdapterNew adapterNew;
+    private ImageView chon;
+    private Dialog dialog;
+
+    final int CROP_PIC = 2;
+    private Uri picUri;
+
+
+    Bitmap thumbnail;
+
+
+    private static final int CAM_REQUEST = 1313;
 
 
     public NewFragment() {
@@ -43,6 +60,36 @@ public class NewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_new, container, false);
         // Init
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        chon = (ImageView) view.findViewById(R.id.chon);
+
+        dialog = new Dialog(view.getContext());
+        dialog.setContentView(R.layout.dialog_getavatar);
+        dialog.setTitle("Choose Avatar Image");
+
+        Button btn_chooseImg = (Button) dialog.findViewById(R.id.btn_choosenGallery);
+        Button btn_takeaphoto = (Button) dialog.findViewById(R.id.btn_choosenTakephoto);
+
+        chon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+
+        btn_chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseFromGallery();
+            }
+        });
+
+        btn_takeaphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                profilepictureOnClick();
+            }
+        });
 
         // Xử lý hiển thị recycler
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false);
@@ -65,5 +112,58 @@ public class NewFragment extends Fragment {
         recyclerView.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener());
 
         return view;
+    }
+
+
+    public void chooseFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+
+
+    private void takeNewProfilePicture(){
+        Fragment profileFrag = this;
+        Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        profileFrag.startActivityForResult(cameraintent, CAM_REQUEST);
+    }
+
+
+    public void profilepictureOnClick(){
+        takeNewProfilePicture();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == CAM_REQUEST) {
+            if(requestCode == CAM_REQUEST){
+                thumbnail = (Bitmap) data.getExtras().get("data");
+
+                //  picUri = data.getData();
+
+                chon.setImageBitmap(thumbnail);
+                dialog.dismiss();
+            }
+        }
+        else if (resultCode == RESULT_OK){
+            picUri = data.getData();
+
+            // Uri targetUri = data.getData();
+            //  textTargetUri.setText(targetUri.toString());
+
+            try {
+                Context applicationContext = MainLayoutActivity.getContextOfApplication();
+                thumbnail = BitmapFactory.decodeStream(applicationContext.getContentResolver().openInputStream(picUri));
+                chon.setImageBitmap(thumbnail);
+
+
+                dialog.dismiss();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
